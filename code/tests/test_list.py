970d6ef814f5aabdb31c323f189f97410ccda4d9
Santexchik2000 <praetoriaixleg@gmail.com>
2020-09-25 16:31:14 +0300

    11

--- code/tests/test_list.py
+++ code/tests/test_list.py
@@ -4,6 +4,15 @@ from random import randint, random
 class Testlist():
 
     def test_1(self):
+        """Тест метода создания list"""
+        list1=list()
+        assert list1==list()
+
+    def test_2(self):
+        """тест метода len на пустом list"""
+        
+
+    def test_3(self):
         """Тест проверки работы метода append и метода len на list"""
         list1=list()
         assert len(list1)==0
@@ -11,13 +20,15 @@ class Testlist():
         with pytest.raises( AssertionError):
             assert len(list1)==-1
     
-    def test_2(self):
+    
+    
+    def test_4(self):
         """Тест операции конкатенации на list"""
         list1=[1,2]
         list2=[3,4]
         assert list1+list2 == [1,2,3,4]
 
-    def test_3(self):
+    def test_5(self):
         """Тест метода реверсии списков и проверка его на list"""
         list1=[1,3]
         list2=[3,1]
@@ -26,14 +37,14 @@ class Testlist():
         assert list1[::-1]==list2
 
     @pytest.mark.parametrize('i', range(1,5))
-    def test_4(self,i, random_number):
+    def test_6(self,i, random_number):
         """Тест параметризованного метода len на list"""
         list1=list()
         list1.append(random_number)
         assert len(list1)==1
     
     @pytest.mark.parametrize('a', range(1,10))
-    def test_5(self,a):
+    def test_7(self,a):
         """Тест метода сортировки на list и его проверка"""
         list1=[randint(0,100) for i in range(1,20)]
         list1.sort()
