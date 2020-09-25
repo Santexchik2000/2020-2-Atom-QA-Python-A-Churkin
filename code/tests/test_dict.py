ece7f0bd016729d734870c05dff739dc3ac6d789
Santexchik2000 <praetoriaixleg@gmail.com>
2020-09-25 16:14:14 +0300

    previous commit for Testdict

--- code/tests/test_dict.py
+++ code/tests/test_dict.py
@@ -4,12 +4,15 @@ from random import randint,random
 class Testdict():
 
     def test_1(self):
+        """Тест методов создания и добавления элемента в список и проверка его наличия в списке"""
+        """Разделить на два теста"""
         dict1=dict()
         dict1.update({1:'one'})
         assert len(dict1)==1
         assert dict1=={1:'one'}
     
     def test_2(self):
+        """Тест метода clear для dict и его проверка"""
         dict1=dict()
         dict1.update({1:'one'})
         assert len(dict1)==1
@@ -17,6 +20,7 @@ class Testdict():
         assert dict1 == dict()
 
     def test_3(self):
+        """Тест метода pop для dict и его проверка"""
         dict1={1:'one',2:'two',3:'three',4:'four',5:'five'}
         assert dict1.get(1)=='one'
         dict1.pop(1)
@@ -25,6 +29,7 @@ class Testdict():
     
     @pytest.mark.parametrize('i', range(0,3))
     def test_4(self,i):
+        """Тест параметризованного метода на dict, удалением элемента и проверки удаления"""
         dict1={a: a for a in range(0,10)}
         assert dict1.get(i) in dict1
         dict1.pop(i)
@@ -33,15 +38,7 @@ class Testdict():
 
     @pytest.mark.parametrize('a', range(0,2))
     def test_5(self,a):
+        """Тест метода сортировки на dict и его проверка"""
         dict1={4:4,3:3,1:1,2:2,5:5,0:0}
         sorted(dict1)
-        assert dict1[a]<dict1[a+1]
-    
-
-
-        
-
-        
-
-
-
+        assert dict1[a]<dict1[a+1] 
